6a65aeb9be7390596dcc604f94310365d36f9c7d
Lewin Lan <lewin.lan.cn@gmail.com>
2022-11-18 20:19:28 +0800

    拆分代码结构

---
+++ src/app/LineChart.data.ts
@@ -0,0 +1,58 @@
+export const aapl: { date: Date; close: number; open?: number }[] = [
+  {
+    date: new Date('2007-04-16'),
+    close: 10,
+  },
+  {
+    date: new Date('2007-04-17'),
+    close: 10,
+    open: -10,
+  },
+  {
+    date: new Date('2007-04-18'),
+    close: 10,
+    open: 8,
+  },
+  {
+    date: new Date('2007-04-19'),
+    close: 10,
+    open: 12,
+  },
+  {
+    date: new Date('2007-04-20'),
+    close: 10,
+    open: 10,
+  },
+  {
+    date: new Date('2007-04-21'),
+    close: 10,
+  },
+  {
+    date: new Date('2007-04-22'),
+    close: 10,
+  },
+  {
+    date: new Date('2007-04-23'),
+    close: 93.24,
+  },
+  {
+    date: new Date('2007-04-24'),
+    close: 1,
+  },
+  {
+    date: new Date('2007-04-25'),
+    close: 95,
+  },
+  {
+    date: new Date('2007-04-26'),
+    close: 200,
+  },
+  {
+    date: new Date('2007-04-27'),
+    close: 96,
+  },
+  {
+    date: new Date('2007-04-28'),
+    close: 97,
+  },
+];
---
+++ src/app/LineChart.ts
@@ -0,0 +1,136 @@
+import * as d3 from 'd3';
+import { aapl } from './LineChart.data';
+
+const config = {
+  width: 500,
+  height: 500,
+  marginTop: 20, // top margin, in pixels
+  marginRight: 30, // right margin, in pixels
+  marginBottom: 30, // bottom margin, in pixels
+  marginLeft: 40, // left margin, in pixels
+  color: 'currentColor', // stroke color of line
+  strokeWidth: 1.5, // stroke width of line, in pixels
+  strokeLinejoin: 'round', // stroke line join of line
+  strokeLinecap: 'round', // stroke line cap of line
+};
+
+export function LineChart(elem: SVGSVGElement): void {
+  const data = aapl;
+  const c = config;
+  const svg = d3
+    .select(elem)
+    .attr('width', c.width)
+    .attr('height', c.height)
+    .attr('viewBox', [0, 0, c.width, c.height]);
+
+  // 线条数据
+  const line1_series = d3.map(data, (d) => d.close);
+  const line2_series = d3.map(data, (d) => d.open);
+
+  // 画 X轴
+  const x_series = d3.map(data, (d) => d.date);
+  const x_domain = d3.extent(x_series);
+  const x_scale = d3.scaleUtc(x_domain, [c.marginLeft, c.width - c.marginRight]);
+  const x_format = d3.timeFormat('%m-%d');
+  const xAxis = d3
+    .axisBottom(x_scale)
+    .ticks(c.width / 80, x_format)
+    .tickSizeOuter(0);
+  svg
+    .append('g')
+    .attr('transform', `translate(0,${c.height - c.marginBottom})`)
+    .call(xAxis);
+
+  // 画 Y轴
+  // const y_series = d3.map(data, (d) => d.close);
+  const y_domain = d3.extent([...d3.extent(line1_series), ...d3.extent(line2_series)]);
+  const y_scale = d3.scaleLinear(y_domain, [c.height - c.marginBottom, c.marginTop]);
+  const y_axis = d3.axisLeft(y_scale).ticks(c.height / 40, null);
+  svg
+    .append('g')
+    .attr('transform', `translate(${c.marginLeft},0)`)
+    .call(y_axis)
+    .call((g) => g.select('.domain').remove())
+    .call((g) => {
+      g.selectAll('.tick line')
+        .clone()
+        .attr('x2', c.width - c.marginLeft - c.marginRight)
+        .attr('stroke-opacity', 0.1);
+    });
+
+  // 画 第一条线
+  const line1_defined = d3.map(data, (d) => d.date && !isNaN(d.close));
+  const line = d3
+    .line<number>()
+    .defined((i) => line1_defined[i])
+    .curve(d3.curveLinear)
+    .x((i) => x_scale(x_series[i]))
+    .y((i) => y_scale(line1_series[i]));
+  svg
+    .append('path')
+    .attr('fill', 'none')
+    .attr('stroke', c.color)
+    .attr('stroke-width', c.strokeWidth)
+    .attr('stroke-linejoin', c.strokeLinejoin)
+    .attr('stroke-linecap', c.strokeLinecap)
+    .attr('d', line(d3.map(data, (_, i) => i)));
+
+  // 画 第二条线
+  const line2_defined = d3.map(data, (d) => d.date && !isNaN(d.open));
+  const line2 = d3
+    .line<number>()
+    .defined((i) => line2_defined[i])
+    .curve(d3.curveLinear)
+    .x((i) => x_scale(x_series[i]))
+    .y((i) => y_scale(line2_series[i]));
+  svg
+    .append('path')
+    .attr('fill', 'none')
+    .attr('stroke', 'red')
+    .attr('stroke-width', c.strokeWidth)
+    .attr('stroke-linejoin', c.strokeLinejoin)
+    .attr('stroke-linecap', c.strokeLinecap)
+    .attr('d', line2(d3.map(data, (_, i) => i)));
+
+  /**
+   * 画 tooltip
+   */
+  const tooltip = svg.append('g').style('pointer-events', 'none');
+
+  const onPointerMove = (event: PointerEvent) => {
+    const x_index = d3.bisectCenter(x_series, x_scale.invert(d3.pointer(event)[0]));
+    tooltip.style('display', null);
+    tooltip.attr('transform', `translate(${x_scale(x_series[x_index])},${y_scale(line1_series[x_index])})`);
+
+    const path = tooltip.selectAll('path').data([undefined]).join('path').attr('fill', 'white').attr('stroke', 'black');
+
+    const text = tooltip
+      .selectAll<SVGTextElement, null>('text')
+      .data([undefined])
+      .join('text')
+      .call((text) =>
+        text
+          .selectAll('tspan')
+          .data([x_format(x_series[x_index]), line1_series[x_index], line2_series[x_index]])
+          .join('tspan')
+          .attr('x', 0)
+          .attr('y', (_, i) => `${i * 1.1}em`)
+          .attr('font-weight', (_, i) => (i ? null : 'bold'))
+          .text((d) => d),
+      );
+
+    const { x, y, width: w, height: h } = text.node().getBBox(); // 根据text的实际尺寸来计算外边框的尺寸和位置
+    text.attr('transform', `translate(${-w / 2},${15 - y})`);
+    path.attr('d', `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+    // svg.property('value', line1_series[x_index]).dispatch('input', { bubbles: true });
+  };
+
+  const onPointerLeave = () => {
+    tooltip.style('display', 'none');
+    // svg.property('value', null).dispatch('input', { bubbles: true });
+  };
+  svg
+    .on('pointerenter pointermove', onPointerMove)
+    .on('pointerleave', onPointerLeave)
+    .on('touchstart', (event) => event.preventDefault());
+}

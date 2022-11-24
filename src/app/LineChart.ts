cc9e75e1b000c3a8a15dac15f3d033756532c59e
Lewin Lan <lewin.lan.cn@gmail.com>
2022-11-24 20:02:14 +0800

    render_Part1 缩放

--- src/app/LineChart.ts
+++ src/app/LineChart.ts
@@ -77,6 +77,7 @@ export class LineChart<DataType extends LineChartData> {
         x_scale.domain(domain);
         brush_element.call(brush.move, null);
         this.render_xAxis();
+        this.render_Part1();
       } else {
         // x_scale.domain(d3.extent(x_series));
       }
@@ -86,15 +87,15 @@ export class LineChart<DataType extends LineChartData> {
       const { x_scale, x_series } = this;
       x_scale.domain(d3.extent(x_series));
       this.render_xAxis();
+      this.render_Part1();
     });
   }
 
   private x_series: number[];
   private x_scale: d3.ScaleTime<number, number, never>;
   private x_format: (date: Date | number) => string;
-  private x_axis: d3.Selection<SVGGElement, unknown, null, undefined>;
   private render_xAxis(): void {
-    const { data, config } = this;
+    const { svg, data, config } = this;
     const { marginLeft, marginRight, marginBottom, width, height } = config;
 
     const x_series = (this.x_series ??= data.items.map((item) => item.timestamp));
@@ -102,18 +103,19 @@ export class LineChart<DataType extends LineChartData> {
     const x_format = (this.x_format ??= d3.timeFormat('%H:%M'));
     const defaultTick = ~~(width / 50);
     const domain = x_scale.domain();
-    const xAxis = d3
+    const x_axis = d3
       .axisBottom(x_scale)
       .ticks(d3.timeMinute.count(domain[0], domain[1]) <= defaultTick ? d3.timeMinute : defaultTick, x_format);
-    if (!this.x_axis) {
-      this.x_axis = this.svg
+    if (!svg.select('.x_axis').size()) {
+      svg
         .append('g')
+        .attr('class', 'x_axis')
         .attr('transform', `translate(0,${height - marginBottom})`)
         .style('color', '#666')
         .style('font-size', 10)
-        .call(xAxis);
+        .call(x_axis);
     } else {
-      this.x_axis.transition().duration(500).call(xAxis);
+      svg.select<SVGGElement>('.x_axis').transition().duration(500).call(x_axis);
     }
   }
 
@@ -122,60 +124,81 @@ export class LineChart<DataType extends LineChartData> {
   private render_Part1(): void {
     const { data, config: c, x_series, x_scale } = this;
     const { marginLeft, marginRight, marginTop, marginBottom, width, height } = c;
-    const part1 = this.svg.append('g');
+    const part1 = this.svg.selectAll<SVGGElement, unknown>('.part1').data([undefined]).join('g').attr('class', 'part1');
 
     /**
      * 裁剪区域
      */
     const part1_bottom = height - marginBottom - Part2_LineHeight * data.part2.length - Part2_MarginTop;
-    const lines_group = part1.append('g');
+    const clip = part1
+      .selectAll('clipPath')
+      .data([undefined])
+      .join('svg:clipPath')
+      .attr('id', 'part1_clip')
+      .selectAll('rect')
+      .data([undefined])
+      .join('svg:rect')
+      .attr('width', width - marginLeft - marginRight)
+      .attr('height', part1_bottom - marginTop)
+      .attr('x', marginLeft)
+      .attr('y', marginTop);
 
     /**
      * 线条数据
      */
-    const part1_series = (this.part1_series = data.part1.map((line) => data.items.map(line.extractor)));
+    const x_domain = x_scale.domain().map((d) => d.valueOf());
+    const items = data.items.filter((item) => item.timestamp >= x_domain[0] && item.timestamp <= x_domain[1]);
+    const part1_series = (this.part1_series = data.part1.map((series) => items.map(series.extractor)));
 
     /**
      * 画 Y轴
      */
-    const y_domain = d3.extent([0, d3.max([d3.max(part1_series.map((s) => d3.max(s))), 10])]);
+    const y_domain = d3.extent([0, 1 + d3.max([d3.max(part1_series.map((s) => d3.max(s))), 10])]);
     const y_scale = (this.part1_y_scale = d3.scaleLinear(y_domain, [part1_bottom, marginTop]));
-    const y_axis = d3.axisLeft(y_scale).ticks(height / 50, null);
+    const y_axis = d3.axisLeft(y_scale).ticks(height / 80, null);
+    if (!part1.selectAll('.y_axis').size()) {
+      part1
+        .append('g')
+        .attr('class', 'y_axis')
+        .attr('transform', `translate(${marginLeft},0)`)
+        .style('color', '#666')
+        .call(y_axis);
+    } else {
+      part1.select<SVGGElement>('.y_axis').transition().duration(500).call(y_axis);
+    }
+    part1.select<SVGGElement>('.y_axis').selectAll('.tick .tickline2').remove();
     part1
-      .append('g')
-      .attr('transform', `translate(${marginLeft},0)`)
-      .style('color', '#666')
-      .call(y_axis)
-      // .call((g) => g.select('.domain').remove())
-      .call((g) => {
-        g.selectAll('.tick line')
-          .clone()
-          .attr('x2', width - marginLeft - marginRight)
-          .attr('stroke-opacity', 0.1);
-      });
+      .select<SVGGElement>('.y_axis')
+      .selectAll('.tick line')
+      .clone()
+      .attr('class', 'tickline2')
+      .attr('x2', width - marginLeft - marginRight)
+      .attr('stroke-opacity', 0.1);
 
     /**
      * 画 线
      */
-    part1_series.forEach((seriesData, index) => {
-      const seriesConfig = data.part1[index];
-      const line_defined = d3.map(seriesData, (d) => !isNaN(d));
-      const line = d3
-        .line<number>()
-        .defined((i) => line_defined[i])
-        // .curve(d3.curveBumpX) // https://github.com/d3/d3/blob/main/API.md#curves
-        .curve(d3.curveMonotoneX)
-        .x((i) => x_scale(x_series[i]))
-        .y((i) => y_scale(seriesData[i]));
-      const line1_path = lines_group
-        .append('path')
-        .attr('fill', 'none')
-        .attr('stroke', seriesConfig.color)
-        .attr('stroke-width', Part1_StrokeWidth)
-        .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-        .attr('d', line(seriesData.map((_, i) => i)));
-    });
+    part1
+      .selectAll<SVGPathElement, number[]>('.series')
+      .data(data.part1)
+      .join('path')
+      .attr('class', 'series')
+      .attr('fill', 'none')
+      .attr('stroke', (seriesConfig) => seriesConfig.color)
+      .attr('stroke-width', Part1_StrokeWidth)
+      .attr('stroke-linejoin', 'round')
+      .attr('stroke-linecap', 'round')
+      .attr('clip-path', 'url(#part1_clip)')
+      .transition()
+      .duration(500)
+      .attr('d', (seriesConfig, index) => {
+        return d3
+          .line<DataType['items'][0]>()
+          .defined((item) => !isNaN(seriesConfig.extractor(item)))
+          .curve(d3.curveMonotoneX)
+          .x((item) => x_scale(item.timestamp))
+          .y((item) => y_scale(seriesConfig.extractor(item)))(data.items);
+      });
   }
 
   private part2_series: number[][];
